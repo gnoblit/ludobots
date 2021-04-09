e1e911f81131a0a8f596af3773bbabb47b9dc9a4
gnoblit <alexnoblit@gmail.com>
2021-04-09 17:25:16 -0400

    unsure

--- generate.py
+++ generate.py
@@ -2,6 +2,6 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+pyrosim.Send_Cube(name="box", pos=[0,0,0.5] , size=[1,1,1])
 
 pyrosim.End()

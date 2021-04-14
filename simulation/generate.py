a2c0b078ea10f424c752c4c5e25b5e7afdff98ec
gnoblit <alexnoblit@gmail.com>
2021-04-14 16:16:52 -0400

    Many links

--- simulation/generate.py
+++ simulation/generate.py
@@ -1,7 +1,23 @@
+from os import X_OK
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="box", pos=[0,0,0.5] , size=[1,1,1])
 
+
+
+for i in range(6):
+    for j in range(6):
+        y0 = 0
+        length = 1
+        width = 1
+        height = 1
+        z0 = .5
+        for k in range(10):
+            pyrosim.Send_Cube(name="box", pos=[i, j, z0] , size=[length,width, height])
+            z0 += 1
+            length *= .9
+            height *= .9
+            width *=.9
+    
 pyrosim.End()
--- simulation/simulate.py
+++ simulation/simulate.py
@@ -1,8 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time 
 
-p.loadSDF("box.sdf")
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+
+
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for x in range(1000):
     time.sleep(1/30)
